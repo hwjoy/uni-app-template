8e9d2d34dba2370759b7822187153586110a8177
hwjoy <hwjoy@163.com>
2023-01-04 17:33:41 +0800

    chore: update template

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import Vue from 'vue'
 import App from './App'
 
+// #ifndef VUE3
+import Vue from 'vue'
 // 设置为 false 以阻止 vue 在启动时生成生产提示
 Vue.config.productionTip = false
 
@@ -10,3 +11,14 @@ const app = new Vue({
   ...App
 })
 app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
